6c3cfe1c03341ff9596be1dee75d2ebf1e54a7d3
ABGEO07 <takalandze2000@mail.ru>
2016-11-20 22:51:08 +0400

    ok

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Test</title>
+        <title>პროცენტები</title>
         <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
     </head>
     <body>
